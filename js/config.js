8850da492d38bc9306c098b35fd44b8107a78f81
mohamedwael667 <69790801+mohamedwael667@users.noreply.github.com>
2026-08-23 20:40:10 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyB5jHB9j497g8vR3NOx9zcXm5YAxKEx_34",
+  authDomain: "admin-el-academy-162b7.firebaseapp.com",
+  projectId: "admin-el-academy-162b7",
+  storageBucket: "admin-el-academy-162b7.firebasestorage.app",
+  messagingSenderId: "333413735925",
+  appId: "1:333413735925:web:ee9dec623b147a6cac94bc",
+  measurementId: "G-8828CV4LZ4"
+});
+const auth = firebase.auth();
+const db = firebase.firestore();
